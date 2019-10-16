d4a4e0eaeadc4f435c42dc00a6f4503da5e95b30
Tamara Trotter <tamaratrotter@Tamaras-iMac.local>
2019-10-15 23:55:24 -0400

    Skeleton for byrdMR App

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-//This component will house Hero Image, Heading Bar and some text with minimal simple css styling
+//This component will house Hero Image, Heading Bar and some text with minimal simple css styling.
